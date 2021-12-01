9d2e48c21d3a1dc256f64c804e8a304d0f324fb3
andrea manzi <andrea.manzi@gmail.com>
2021-12-01 20:19:46 +0000

    fix formatting

--- plugins/xrootd_probe.py
+++ plugins/xrootd_probe.py
@@ -29,11 +29,16 @@ PROBE_VERSION = "v0.0.1"
 
 
 # ########################################################################### #
-app = nap.core.Plugin(description="NAGIOS XRootD probe",
-                      version=PROBE_VERSION)
+app = nap.core.Plugin(description="NAGIOS XRootD probe", version=PROBE_VERSION)
 app.add_argument("-E", "--endpoint", help="base URL to test")
 app.add_argument("-X", "--x509", help="location of x509 certificate proxy file")
-app.add_argument("--se-timeout",dest="se_timeout", type=int, help="storage operations timeout", default=60)
+app.add_argument(
+    "--se-timeout",
+    dest="se_timeout",
+    type=int,
+    help="storage operations timeout",
+    default=60,
+)
 
 gfal2.set_verbose(gfal2.verbose_level.normal)
 
@@ -41,9 +46,9 @@ gfal2.set_verbose(gfal2.verbose_level.normal)
 workdir_metric = tempfile.mkdtemp()
 
 # files and patterns
-_fileTest = workdir_metric + '/testFile.txt'
-_fileTestIn = workdir_metric + '/testFileIn.txt'
-_filePattern = 'testfile-put-%s-%s.txt'  # time, uuid
+_fileTest = workdir_metric + "/testFile.txt"
+_fileTestIn = workdir_metric + "/testFileIn.txt"
+_filePattern = "testfile-put-%s-%s.txt"  # time, uuid
 
 _fileDictionary = {}
 
@@ -53,40 +58,45 @@ ctx = gfal2.creat_context()
 # GFAL version
 gfal2_ver = "gfal2 " + gfal2.get_version()
 
+
 def parse_args(args, io):
     if not args.endpoint:
         return 1
     if args.x509:
-        cred = gfal2.cred_new("X509_CERT",args.x509)
-        gfal2.cred_set(ctx,"root://",cred)
-        gfal2.cred_set(ctx,"xroot://",cred)
- 
+        cred = gfal2.cred_new("X509_CERT", args.x509)
+        gfal2.cred_set(ctx, "root://", cred)
+        gfal2.cred_set(ctx, "xroot://", cred)
+
 
 @app.metric(seq=1, metric_name="LsDir", passive=True)
 def metricLsDir(args, io):
     """
     List content of the top level folder using gfal2.listdir().
     """
-    if parse_args(args,io):
+    if parse_args(args, io):
         io.status = nap.CRITICAL
-        io.summary = 'Argument Endpoint (-E, --endpoint) is missing'
+        io.summary = "Argument Endpoint (-E, --endpoint) is missing"
         return
     try:
         ctx.listdir(str(args.endpoint))
-        io.summary = 'Storage Path[%s] Directory successfully listed' % str(args.endpoint)
+        io.summary = "Storage Path[%s] Directory successfully listed" % str(
+            args.endpoint
+        )
         io.status = nap.OK
     except gfal2.GError as e:
         er = e.message
         if er:
             io.status = nap.CRITICAL
-            io.summary = '[Err:%s];' %  str(er)
+            io.summary = "[Err:%s];" % str(er)
         else:
             io.status = nap.CRITICAL
-            io.summary = 'Error'
+            io.summary = "Error"
     except Exception as e:
         io.set_status(
-        nap.CRITICAL, 'problem invoking gfal2 listdir(): %s:%s' %
-        (str(e), sys.exc_info()[0]))
+            nap.CRITICAL,
+            "problem invoking gfal2 listdir(): %s:%s" % (str(e), sys.exc_info()[0]),
+        )
+
 
 @app.metric(seq=2, metric_name="Put", passive=True)
 def metricPut(args, io):
@@ -94,7 +104,7 @@ def metricPut(args, io):
 
     # verify lsdir test succeeded
     results = app.metric_results()
-    if (results[0][1] != nap.OK ):
+    if results[0][1] != nap.OK:
         io.set_status(nap.WARNING, "lsdir skipped")
         return
 
@@ -103,16 +113,16 @@ def metricPut(args, io):
         src_file = _fileTest
         fp = open(src_file, "w")
         for s in "1234567890":
-            fp.write(s + '\n')
+            fp.write(s + "\n")
         fp.close()
 
-        fn = _filePattern % (str(int(time.time())),str(uuid.uuid1()))
+        fn = _filePattern % (str(int(time.time())), str(uuid.uuid1()))
 
-        dest_file = args.endpoint + '/' + fn
-        _fileDictionary[args.endpoint] ={}
-        _fileDictionary[args.endpoint]['fn'] = fn
+        dest_file = args.endpoint + "/" + fn
+        _fileDictionary[args.endpoint] = {}
+        _fileDictionary[args.endpoint]["fn"] = fn
     except IOError as e:
-        io.set_status(nap.CRITICAL, 'Error creating source file')
+        io.set_status(nap.CRITICAL, "Error creating source file")
 
     # Set transfer parameters
     params = ctx.transfer_parameters()
@@ -120,7 +130,7 @@ def metricPut(args, io):
     params.timeout = args.se_timeout
     start_transfer = datetime.datetime.now()
 
-    stMsg = 'File was copied to the XRootD endpoint'
+    stMsg = "File was copied to the XRootD endpoint"
 
     try:
         ctx.filecopy(params, "file://" + str(src_file), str(dest_file))
@@ -131,55 +141,56 @@ def metricPut(args, io):
         io.status = nap.CRITICAL
         er = e.message
         if er:
-            io.summary = '[Err:%s]' % str(er)
+            io.summary = "[Err:%s]" % str(er)
         else:
-            io.summary = 'Error'
+            io.summary = "Error"
     except Exception as e:
         io.set_status(
-            nap.CRITICAL, 'problem invoking gfal2 filecopy(): %s:%s' %
-            (str(e), sys.exc_info()[0]))
+            nap.CRITICAL,
+            "problem invoking gfal2 filecopy(): %s:%s" % (str(e), sys.exc_info()[0]),
+        )
 
 
 @app.metric(seq=3, metric_name="Ls", passive=True)
 def metricLs(args, io):
     """Stat (previously copied) file(s) on the XRootd endpoint."""
 
-     # verify previous test succeeded
+    # verify previous test succeeded
     results = app.metric_results()
-    if ( results[1][1] != nap.OK ):
+    if results[1][1] != nap.OK:
         io.set_status(nap.WARNING, "VOLs skipped")
         return
 
     if len(_fileDictionary.keys()) == 0:
-        io.set_status(nap.WARNING, 'No endpoints found to test')
+        io.set_status(nap.WARNING, "No endpoints found to test")
         return
 
     endpoints = []
 
     for endpt in _fileDictionary.keys():
-        dest_filename = (_fileDictionary[endpt])['fn']
-        dest_file = endpt + '/' + dest_filename
+        dest_filename = (_fileDictionary[endpt])["fn"]
+        dest_file = endpt + "/" + dest_filename
         endpoints.append(dest_file)
 
     for url in endpoints:
         try:
             statp = ctx.stat(str(url))
 
-            io.summary = 'File successfully listed'
+            io.summary = "File successfully listed"
             io.status = nap.OK
         except gfal2.GError as e:
             er = e.message
             io.status = nap.CRITICAL
             if er:
-                io.summary = '[Err:%s];' % str(er)
+                io.summary = "[Err:%s];" % str(er)
             else:
-                io.summary = 'Error'
-       
+                io.summary = "Error"
+
         except Exception as e:
             io.set_status(
-                nap.CRITICAL, 'problem invoking gfal2 stat(): %s:%s' %
-                (str(e), sys.exc_info()[0]))
-
+                nap.CRITICAL,
+                "problem invoking gfal2 stat(): %s:%s" % (str(e), sys.exc_info()[0]),
+            )
 
 
 @app.metric(seq=4, metric_name="Get", passive=True)
@@ -188,20 +199,20 @@ def metricGet(args, io):
 
     # verify previous test succeeded
     results = app.metric_results()
-    if ( results[2][1] != nap.OK ):
+    if results[2][1] != nap.OK:
         io.set_status(nap.WARNING, "Get skipped")
         return
 
     if len(_fileDictionary.keys()) == 0:
-        io.set_status(nap.WARNING, 'No endpoints found to test')
+        io.set_status(nap.WARNING, "No endpoints found to test")
         return
 
     for endpt in _fileDictionary.keys():
 
-        src_filename = (_fileDictionary[endpt])['fn']
-        src_file = endpt + '/' + src_filename
+        src_filename = (_fileDictionary[endpt])["fn"]
+        src_file = endpt + "/" + src_filename
 
-        dest_file = 'file://' + _fileTestIn
+        dest_file = "file://" + _fileTestIn
 
         # Set transfer parameters
         params = ctx.transfer_parameters()
@@ -209,7 +220,7 @@ def metricGet(args, io):
 
         params.overwrite = True
 
-        stMsg = 'File was copied from XRootD.'
+        stMsg = "File was copied from XRootD."
         start_transfer = datetime.datetime.now()
         try:
             ctx.filecopy(params, str(src_file), str(dest_file))
@@ -217,23 +228,30 @@ def metricGet(args, io):
                 # Files match
                 io.status = nap.OK
                 total_transfer = datetime.datetime.now() - start_transfer
-                io.summary = stMsg + ': Diff successful.' + " Transfer time: " + str(total_transfer)
+                io.summary = (
+                    stMsg
+                    + ": Diff successful."
+                    + " Transfer time: "
+                    + str(total_transfer)
+                )
             else:
                 # Files do not match
                 io.status = nap.CRITICAL
-                io.summary = 'Files differ!'
+                io.summary = "Files differ!"
 
         except gfal2.GError as e:
             io.status = nap.CRITICAL
             er = e.message
             if er:
-                io.summary = '[Err:%s]' % str(er)
+                io.summary = "[Err:%s]" % str(er)
             else:
-                io.summary = 'Error'
+                io.summary = "Error"
         except Exception as e:
             io.set_status(
-                nap.CRITICAL, 'problem invoking gfal2 filecopy(): %s:%s' %
-                (str(e), sys.exc_info()[0]))
+                nap.CRITICAL,
+                "problem invoking gfal2 filecopy(): %s:%s"
+                % (str(e), sys.exc_info()[0]),
+            )
 
 
 @app.metric(seq=5, metric_name="Del", passive=True)
@@ -242,18 +260,18 @@ def metricDel(args, io):
 
     # skip only if the put failed
     results = app.metric_results()
-    if ( results[3][1] != nap.OK ):
+    if results[3][1] != nap.OK:
         io.set_status(nap.WARNING, "Del skipped")
         return
 
     if len(_fileDictionary.keys()) == 0:
-        io.set_status(nap.CRITICAL, 'No endpoints found to test')
+        io.set_status(nap.CRITICAL, "No endpoints found to test")
 
     for endpt in _fileDictionary.keys():
 
-        src_filename = (_fileDictionary[endpt])['fn']
-        src_file = endpt + '/' + src_filename
-        stMsg = 'File was deleted from the XRootD endpoint.'
+        src_filename = (_fileDictionary[endpt])["fn"]
+        src_file = endpt + "/" + src_filename
+        stMsg = "File was deleted from the XRootD endpoint."
         try:
             ctx.unlink(str(src_file))
             io.status = nap.OK
@@ -261,14 +279,16 @@ def metricDel(args, io):
         except gfal2.GError as e:
             er = e.message
             if er:
-                io.summary = '[Err:%s]' % str(er)
+                io.summary = "[Err:%s]" % str(er)
             else:
-                io.summary = 'Error'
+                io.summary = "Error"
             io.status = nap.CRITICAL
         except Exception as e:
             io.set_status(
-                nap.CRITICAL, 'problem invoking gfal2 unlink(): %s:%s' %
-                (str(e), sys.exc_info()[0]))
+                nap.CRITICAL,
+                "problem invoking gfal2 unlink(): %s:%s" % (str(e), sys.exc_info()[0]),
+            )
+
 
 @app.metric(seq=6, metric_name="All", passive=False)
 def metricAlll(args, io):
@@ -277,7 +297,7 @@ def metricAlll(args, io):
     results = app.metric_results()
 
     statuses = [e[1] for e in results]
-    
+
     if all(st == 0 for st in statuses):
         io.set_status(nap.OK, "All fine")
     elif nap.CRITICAL in statuses:
@@ -291,5 +311,5 @@ def metricAlll(args, io):
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
